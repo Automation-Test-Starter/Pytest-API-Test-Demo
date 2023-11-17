ca740fb2af0a1d5636dfed7932916d8d0702e82a
naodeng <nao.deng@thoughtworks.com>
2023-11-17 11:58:03 +0800

    add demo for multi env support

--- conftest.py
+++ conftest.py
@@ -3,22 +3,10 @@ import json
 import json
 import os
 
-# def env_config(request):
-#     environment = request.config.getoption("--environment")
-#     if environment == "dev":
-#         # 在开发环境中执行测试
-#         pass
-#     elif environment == "test":
-#         # 在测试环境中执行测试
-#         pass
-#     elif environment == "prod":
-#         # 在生产环境中执行测试
-#         pass
-
 
 @pytest.fixture(scope="session")
 def env_config(request):
-    # 根据命令行参数选择环境配置文件
+    # get config file from different env
     env = os.getenv('ENV', 'dev')
     with open(f'config/{env}_config.json', 'r') as config_file:
         config = json.load(config_file)
@@ -27,17 +15,17 @@ def env_config(request):
 
 @pytest.fixture(scope="session")
 def env_request_data(request):
-    # 根据命令行参数选择环境request_data文件
+    # get request data file from different env
     env = os.getenv('ENV', 'dev')
-    with open(f'data/{env}_request_test_data.json', 'r') as request_data_file:
+    with open(f'data/{env}_request_data.json', 'r') as request_data_file:
         request_data = json.load(request_data_file)
     return request_data
 
 
 @pytest.fixture (scope="session")
 def env_response_data(request):
-    # 根据命令行参数选择环境response_data文件
+    # get response data file from different env
     env = os.getenv('ENV', 'dev')
-    with open(f'data/{env}_response_test_data.json', 'r') as response_data_file:
+    with open(f'data/{env}_response_data.json', 'r') as response_data_file:
         response_data = json.load(response_data_file)
     return response_data
